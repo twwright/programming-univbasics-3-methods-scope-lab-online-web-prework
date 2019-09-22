d4baed2f44e98b25623258f83fa67e93ee4eeec7
Thomas Wright <twwright@gmail.com>
2019-09-22 17:03:49 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,12 @@ def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
+
+def link
+  ohdear = "It's Dangerous To Go Alone! Take this."
+  puts ohdear
+end
+
+def any_phrase(phrase)
+  return "Do A Barrell Roll!\n""
+end
